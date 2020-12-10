241abd51ef1b4b57c8583da05a5ff3654cfb935e
Marcos Yacob <marcos.yacob@hpe.com>
2020-12-10 12:05:34 -0300

    add environment updates to update function
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- update-function.sh
+++ update-function.sh
@@ -4,8 +4,6 @@ ARN=$(cat .layer.arn.txt)
 
 . 00-vars.sh
 
-echo "$AWS_REGION"
-
 echo "Updating function"
 aws lambda update-function-code \
 	--function-name "${FUNCTION_NAME}" \
@@ -14,6 +12,7 @@ aws lambda update-function-code \
 aws lambda update-function-configuration \
 	--function-name "${FUNCTION_NAME}" \
 	--region "${AWS_REGION}" \
+	--environment "Variables={SECRET_NAME=${SECRET_NAME}}" \
 	--layers "${ARN}"
 aws lambda publish-version \
 	--function-name "${FUNCTION_NAME}" \
